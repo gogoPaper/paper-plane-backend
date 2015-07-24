efe8c84a342ebca8da06e5a7288da436353185a9
unknown <moxhe@moxhe-PC0.tencent.com>
2015-07-24 13:45:41 +0800

    init project

---
+++ models/user.py
@@ -0,0 +1,17 @@
+
+
+class User:
+    # {
+    #     id: 'xxx',
+    #     token: 'xxx',
+    #     phone: 'xxx',
+    #     password: 'xxx',
+    #     nickname: 'xxx',
+    #     avatar: 'url',
+    #     focus_storis: [story_ids],
+    #     focus_users?: [user_ids],
+    #     degree: <number>,
+    #     experience_value: <number>
+    # }
+    def __init__(self, arg):
+        self.arg = arg
